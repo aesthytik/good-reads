49aca5c47798600989da0c09e4e2880602df2762
Vipin Kumar Rawat <rawat.vipin4818@gmail.com>
2025-05-06 10:41:13 +0100

    Refactor App component; remove commented-out header and category navigation for cleaner code and improved readability.

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,9 @@ import BookSearch from "./book-search/BookSearch";
 function App() {
   return (
     <div className="app-container">
-      {/* Skip to main content link for keyboard users */}
       <a href="#main-content" className="skip-link">
         Skip to main content
       </a>
-
-      {/* <header className="header" role="banner">
-        <div className="header--content">
-          <div className="header-top">
-            <div className="header-left">
-              <div className="logo">
-                <span className="logo-text">
-                  my<span className="logo-highlight">GoodReads</span>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header> */}
-
-      {/* Category navigation */}
 
       {/* Main content area */}
       <main id="main-content" tabIndex={-1}>
